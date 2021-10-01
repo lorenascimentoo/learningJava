a69caf7d8b9688507f2e38cd1f2c812e624ee846
Lorena Nascimento <lorenanascimento28@gmail.com>
2021-10-01 20:43:01 -0300

    algumas regras e convencoes

--- MeuPrimeiroPrograma.java
+++ MeuPrimeiroPrograma.java
@@ -1,5 +1,12 @@
-class MeuPrimeiroPrograma{
-    public static void main(String[] args){
+class MeuPrimeiroPrograma{ //inicio do escopo da classe
+    public static void main(String[] args){ 
         System.out.println("O primeiro de muitos!");
+
+        //convenção camelCase
+        // as palavras iniciam com letra maiuscula e, quando necessario, tem seu case alterado
+        // exemplo: EsseNomeDeClasse
+        
+        //um arquivo .java define uma classe é recomendado que o arquivo e a classe tenham o mesmo nome para evitar erros na executar o codigo
+        // java é case sensitive, casa é diferente de Casa
     }
-}
+}//final do escopo da classe
