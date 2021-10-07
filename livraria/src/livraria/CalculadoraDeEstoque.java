ec7393befc292566ead308aacbea0dba3554285f
Lorena Nascimento <lorenanascimento28@gmail.com>
2021-10-07 18:00:06 -0300

    declaracao e atribuicao de variaveis

---
+++ livraria/src/livraria/CalculadoraDeEstoque.java
@@ -0,0 +1,22 @@
+package livraria;
+
+public class CalculadoraDeEstoque {
+
+	public static void main(String[] args) {
+		
+		// Ao declarar um variavel, solicitamos que seja feita a reserva de espaço em uma regiao da memoria
+		// para futuramente armazenar esses valores
+		// ao declarar um variavel indicamos o seu tipo, é obrigatório que ela tenha um
+		// o sinal = indica a atribuicao de um valor para a variavel
+		double livroJava8 = 59.90;
+		double livroTDD = 59.90;
+		
+		double soma = livroJava8 + livroTDD;
+		
+		System.out.println("O total em estoque é: "+soma);
+		
+		// OPERADORES ARITMETICOS EM JAVA
+		// + soma | - subtracao | * multiplicacao | / divisao | % modulo
+	}
+
+}
